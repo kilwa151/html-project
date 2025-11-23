ca68ac1dd37dc2a9f7408ffb4cfc3dfd071fe580
kanza <146678467+kilwa151@users.noreply.github.com>
2025-11-23 15:33:56 +0100

    Add files via upload

---
+++ exxercice 1 tp5.jsx
@@ -0,0 +1,10 @@
+//exercice 1:
+
+let a = Number(prompt("Entrez le premier nombre :"));
+let b = Number(prompt("Entrez le deuxième nombre :"));
+
+console.log("Somme : " + (a + b));
+console.log("Différence : " + (a - b));
+console.log("Produit : " + (a * b));
+console.log("Quotient : " + (a / b)); 
+
